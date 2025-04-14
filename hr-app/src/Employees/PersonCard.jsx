5878dbed5a7388a09c35b24da2a72eb95c2827d6
Huttunenbe <beniittahuttunen@gmail.com>
2025-04-14 16:48:39 +0300

    deleted unneeded div and div class

--- hr-app/src/Employees/PersonCard.jsx
+++ hr-app/src/Employees/PersonCard.jsx
@@ -1,29 +1,27 @@
-import './employees.css';
+import "./employees.css";
 
 const PersonCard = (props) => {
   return (
     <div className="box">
-      <div className="box2">
-        <img src={props.departmentImage} id="departmentPicture" />
+      <img src={props.departmentImage} id="departmentPicture" />
 
-        <p className="ID">ID: {props.id}</p>
-        <p>Name: {props.name}</p>
-        <p>Title: {props.title}</p>
-        <p>Salary: {props.salary}€</p>
-        <p>Phone: {props.phone}</p>
-        <p>Email: {props.email}</p>
-        <p>
-          Favorite Animal: {props.animal} {props.animalEmoji}
-        </p>
-        <p>Start Date: {props.startDate}</p>
-        <p>Location: {props.location}</p>
-        <p>Department: {props.department}</p>
-        <div className="Skills">
-          <p> Skills: {props.skills}</p>
-        </div>
-        <p>Years in Service: {props.yearsInService.toFixed(1)} years</p>
-        <p>{props.reminderEmoji}</p>
+      <p className="ID">ID: {props.id}</p>
+      <p>Name: {props.name}</p>
+      <p>Title: {props.title}</p>
+      <p>Salary: {props.salary}€</p>
+      <p>Phone: {props.phone}</p>
+      <p>Email: {props.email}</p>
+      <p>
+        Favorite Animal: {props.animal} {props.animalEmoji}
+      </p>
+      <p>Start Date: {props.startDate}</p>
+      <p>Location: {props.location}</p>
+      <p>Department: {props.department}</p>
+      <div className="Skills">
+        <p> Skills: {props.skills}</p>
       </div>
+      <p>Years in Service: {props.yearsInService.toFixed(1)} years</p>
+      <p>{props.reminderEmoji}</p>
     </div>
   );
 };
