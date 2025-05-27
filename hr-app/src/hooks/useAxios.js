65dec8644825401dd60f644080e5df1eca53c2f4
Huttunenbe <beniittahuttunen@gmail.com>
2025-05-27 15:47:38 +0300

    add env file

--- hr-app/src/hooks/useAxios.js
+++ hr-app/src/hooks/useAxios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://hr-app-backend-1-esbm.onrender.com";
+const BASE_URL = "process.env.REACT_APP_API_URL";
 
 const useAxios = () => {
   const get = (url) => axios.get(BASE_URL + url);
