78c19fb75685a91309590c1a43bb855a63261388
Huttunenbe <beniittahuttunen@gmail.com>
2025-07-10 11:11:10 +0300

    add space before employee info and alert for adding employees

--- hr-app/src/pages/addEmployee/EmployeeForm.jsx
+++ hr-app/src/pages/addEmployee/EmployeeForm.jsx
@@ -1,51 +1,89 @@
-const EmployeeForm=({handleChange,handleSubmit,employeesData})=>{return(<form onSubmit={handleSubmit}><h3>Enter Employee Details</h3><label htmlFor="name">name:</label><input
-type="text"
-name="name"
-value={employeesData.name}
-onChange={handleChange}
-placeholder="Name"/><label htmlFor="title">title</label><input
-type="text"
-name="title"
-value={employeesData.title}
-onChange={handleChange}
-placeholder="Title"/><label htmlFor="salary">salary:</label><input
-name="salary"
-type="number"
-value={employeesData.salary}
-onChange={handleChange}
-placeholder="Salary"/><label htmlFor="phone">phone</label><input
-name="phone"
-type="number"
-value={employeesData.phone}
-onChange={handleChange}
-placeholder="Phone"/><label htmlFor="email">email</label><input
-name="email"
-type="text"
-value={employeesData.email}
-onChange={handleChange}
-placeholder="Email"/><label htmlFor="animal">animal</label><input
-name="animal"
-type="text"
-value={employeesData.animal}
-onChange={handleChange}
-placeholder="Animal"/><label htmlFor="startDate">start date</label><input
-name="startDate"
-type="date"
-value={employeesData.startDate}
-onChange={handleChange}
-placeholder="Start Date"/><label htmlFor="location">location</label><input
-name="location"
-type="text"
-value={employeesData.location}
-onChange={handleChange}
-placeholder="Location"/><label htmlFor="department">depatment</label><input
-name="department"
-type="text"
-value={employeesData.department}
-onChange={handleChange}
-placeholder="Department"/><label htmlFor="skills">skills</label><input
-name="skills"
-type="text"
-value={employeesData.skills}
-onChange={handleChange}
-placeholder="Skills"/><button type="submit">Add Employee</button></form>)};export default EmployeeForm
+const EmployeeForm = ({ handleChange, handleSubmit, employeesData }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Enter Employee Details</h3>
+      <label htmlFor="name">name:</label>
+      <input
+        type="text"
+        name="name"
+        value={employeesData.name}
+        onChange={handleChange}
+        placeholder="Name"
+      />
+      <label htmlFor="title">title</label>
+      <input
+        type="text"
+        name="title"
+        value={employeesData.title}
+        onChange={handleChange}
+        placeholder="Title"
+      />
+      <label htmlFor="salary">salary:</label>
+      <input
+        name="salary"
+        type="number"
+        value={employeesData.salary}
+        onChange={handleChange}
+        placeholder="Salary"
+      />
+      <label htmlFor="phone">phone</label>
+      <input
+        name="phone"
+        type="number"
+        value={employeesData.phone}
+        onChange={handleChange}
+        placeholder="Phone"
+      />
+      <label htmlFor="email">email</label>
+      <input
+        name="email"
+        type="text"
+        value={employeesData.email}
+        onChange={handleChange}
+        placeholder="Email"
+      />
+      <label htmlFor="animal">animal</label>
+      <input
+        name="animal"
+        type="text"
+        value={employeesData.animal}
+        onChange={handleChange}
+        placeholder="Animal"
+      />
+      <label htmlFor="startDate">start date</label>
+      <input
+        name="startDate"
+        type="date"
+        value={employeesData.startDate}
+        onChange={handleChange}
+        placeholder="Start Date"
+      />
+      <label htmlFor="location">location</label>
+      <input
+        name="location"
+        type="text"
+        value={employeesData.location}
+        onChange={handleChange}
+        placeholder="Location"
+      />
+      <label htmlFor="department">depatment</label>
+      <input
+        name="department"
+        type="text"
+        value={employeesData.department}
+        onChange={handleChange}
+        placeholder="Department"
+      />
+      <label htmlFor="skills">skills</label>
+      <input
+        name="skills"
+        type="text"
+        value={employeesData.skills}
+        onChange={handleChange}
+        placeholder="Skills"
+      />
+      <button type="submit">Add Employee</button>
+    </form>
+  );
+};
+export default EmployeeForm;
--- hr-app/src/pages/personLIst/PersonList.jsx
+++ hr-app/src/pages/personLIst/PersonList.jsx
@@ -1,1 +1,41 @@
-import Header from "../../components/Header/Header";import Footer from "../../components/Footer/Footer";import PersonCard from "../../components/Personcard/PersonCard";import DotLoader from "react-spinners/DotLoader";import{calculateYearsInService,schedulReminder,animalToEmoji}from "./PersonListFunctions";import GetDepartmentImage from "../../components/Employees/DepartmentImages";const PersonList=({employeeData,onEdit})=>{return(<div><Header appName="Hr App"/><h2>Employee list</h2><main><div className="employeeTickets">{employeeData.map((employee)=>{const yearsInService=calculateYearsInService(employee);const reminderEmoji=schedulReminder(yearsInService);const animalEmoji=animalToEmoji(employee.animal);const departmentImage=GetDepartmentImage(employee.department);return(<div key={employee.id}className="message"><PersonCard{...employee}reminderEmoji={reminderEmoji}animalEmoji={animalEmoji}yearsInService={yearsInService}departmentImage={departmentImage}onEdit={onEdit}/></div>)})}</div></main><Footer className="REACT25"/></div>)};export default PersonList
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import PersonCard from "../../components/Personcard/PersonCard";
+import {
+  calculateYearsInService,
+  schedulReminder,
+  animalToEmoji,
+} from "./PersonListFunctions";
+import GetDepartmentImage from "../../components/Employees/DepartmentImages";
+const PersonList = ({ employeeData, onEdit }) => {
+  return (
+    <div>
+      <Header appName="Hr App" />
+      <h2>Employee list</h2>
+      <main>
+        <div className="employeeTickets">
+          {employeeData.map((employee) => {
+            const yearsInService = calculateYearsInService(employee);
+            const reminderEmoji = schedulReminder(yearsInService);
+            const animalEmoji = animalToEmoji(employee.animal);
+            const departmentImage = GetDepartmentImage(employee.department);
+            return (
+              <div key={employee.id} className="message">
+                <PersonCard
+                  {...employee}
+                  reminderEmoji={reminderEmoji}
+                  animalEmoji={animalEmoji}
+                  yearsInService={yearsInService}
+                  departmentImage={departmentImage}
+                  onEdit={onEdit}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </main>
+      <Footer className="REACT25" />
+    </div>
+  );
+};
+export default PersonList;
